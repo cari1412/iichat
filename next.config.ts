13f353edf3555f5938d67bdafdff9a9844f16308
cari1412 <vadis1766@gmail.com>
2025-01-13 00:00:06 +0300

    asw

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
-};
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',  // Включаем статическую генерацию
+  basePath: '/iichat', // Замените на название вашего репозитория
+  images: {
+    unoptimized: true,
+  },
+  assetPrefix: '/iichat/', // Замените на название вашего репозитория
+}
 
 export default nextConfig;
